01ad3d4031d5ba57f271373015e2ffd7cb637c9a
0x00A3 <56656809+0x00A3@users.noreply.github.com>
2021-02-01 01:07:23 +0100

    Update yt.sh

--- yt.sh
+++ yt.sh
@@ -3,6 +3,9 @@ clear
 BLUE=`tput setaf 6`
 WHITE=`tput setaf 7`
 reset=`tput sgr0`
+apt-get install youtube-dl
+pip3 install youtube-dl
+clear
 echo -e "${WHITE}-----------------------------------------------------------------------------------"
 echo -e "${BLUE}                             [+] Made By Birdlinux [+]                               "
 echo -e "${WHITE}-----------------------------------------------------------------------------------"
